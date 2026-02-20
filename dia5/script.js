ecdc288ebeb72d240a1b0d977f7e9d1c3c68f8c1
Santiago Rueda Garcia <santirueda0917@gmail.com>
2026-02-20 06:50:34 -0500

    :|

--- dia5/script.js
+++ dia5/script.js
@@ -1,8 +1,10 @@
 let nombre;
-document.getElementById("id_or_name").addEventListener("keydown",(e)=>{
+const LINK = "https://pokeapi.co/api/v2/pokemon/"
+let linksito;
+const buscador = document.getElementById("id_or_name").addEventListener("keydown",(e)=>{
     if(e.key === 'Enter'){
         nombre = document.getElementById("id_or_name").value;
-        let linksito="https://pokeapi.co/api/v2/pokemon/"+nombre;
+        linksito = LINK + nombre;
         console.log(linksito);
         fetch(linksito)
         .then(function (res){return res.json()})
@@ -14,20 +16,55 @@ document.getElementById("id_or_name").addEventListener("keydown",(e)=>{
                 
                 contenedor.innerHTML+=`
                     <div class="card">
-                        <img src="${data["sprites"]['other']['showdown']['front_default']}" alt="">
+                        <img src="${imprimirImagen(data)}" alt="">
                     </div>
                 `;
             }else{
                 contenedor.innerHTML="<p>No hay resultados</p>";
             }
-        
-    })
-        
-    }
-   
+    })    
+    } 
 })
+
 document.getElementById('Next').addEventListener("click", function(){
-    if(nombre){
-        
+    averiguar_siguiente(linksito)
+});
+
+function averiguar_siguiente(link){
+    let newLink
+    fetch(link)
+    .then(function (res){return res.json()})
+    .then((data)=>{
+        let id = data['id']
+        newLink = LINK + (id+1)
+        siguiente(newLink)
+    })
+}
+
+function siguiente(link){
+    fetch(link)
+    .then(function (res){return res.json()})
+    .then((data)=>{
+        console.log(data);
+            let contenedor = document.getElementById("pokemon-result");
+            contenedor.innerHTML="";
+            if(data){
+                
+                contenedor.innerHTML+=`
+                    <div class="card">
+                        <img src="${imprimirImagen(data)}" alt="">
+                    </div>
+                `;
+            }else{
+                contenedor.innerHTML="<p>No hay resultados</p>";
+            }
+    })
+}
+
+function imprimirImagen(datos){
+    if(datos["sprites"]['other']['showdown']['front_default'] == null){
+        return datos['sprites']['front_default']
+    }else{
+        return datos["sprites"]['other']['showdown']['front_default']
     }
-})
+};
